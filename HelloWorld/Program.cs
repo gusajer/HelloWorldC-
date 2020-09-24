2c4044f1955760a8e0734e49fb41abeca2ce1eba
Gustav <70881859+gusajer@users.noreply.github.com>
2020-09-24 13:37:41 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static int[] tal = new int[] { 11, 100, 66, 90, 80, 88, 120, 12 };
+        static void Main(string[] args)
+        {
+            OddEven lala = new OddEven();
+            lala.Even(tal);
+ 
+        }
+    }
+}
+
+class OddEven
+{
+    public void Even(int[] array)
+    {
+
+        for (int i = 0; i < array.Length ; i++)
+        {
+            if (array[i] % 2 != 0)
+            {
+                    Console.Write(array[i] + "\n");
+
+            }
+
+        }
+       
+    }
+}
